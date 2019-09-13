8718b4869fd1f869009cf67b04398001cae34cd1
Neptune <eugeneogongo@live.com>
2019-09-13 14:05:33 +0300

    Solution Created

---
+++ EmployeeHierarchy/CustomException/ManagerMoreThanOne.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace EmployeeHierarchy.CustomException
+{
+    class ManagerMoreThanOne : Exception
+    {
+        public ManagerMoreThanOne()
+        {
+        }
+
+        public override Exception GetBaseException()
+        {
+            return base.GetBaseException();
+        }
+
+        public override string Message { get; }
+
+        public ManagerMoreThanOne(string message) : base(message)
+        {
+        }
+
+        public ManagerMoreThanOne(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ManagerMoreThanOne(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
